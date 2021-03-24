509ae3aacef56df581710a5761191c2a50d74f1d
pcontra <pedro.andrade@sbanken.no>
2021-03-24 13:09:09 +0000

    Configured webpack

--- aulas/reactjs/01-github-explorer/src/index.js
+++ aulas/reactjs/01-github-explorer/src/index.js
@@ -1,5 +1,2 @@
 import React from "react";
-
-function App() {
-  return <h1>Hello from the other side!</h1>
-}
+import { App } from "./App";
---
+++ aulas/reactjs/01-github-explorer/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+    ],
+  },
+};
