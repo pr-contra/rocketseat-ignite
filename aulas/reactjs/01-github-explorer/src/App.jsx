fd3bf047b0d91718db0b12d0f364058ccefdd880
pcontra <pedro.andrade@sbanken.no>
2021-03-28 14:27:39 +0100

    Configured webpack-dev-server

--- aulas/reactjs/01-github-explorer/src/App.jsx
+++ aulas/reactjs/01-github-explorer/src/App.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function App() {
   return <h1>Hello from the other side!</h1>
 }
--- aulas/reactjs/01-github-explorer/src/index.js
+++ aulas/reactjs/01-github-explorer/src/index.js
@@ -1,2 +1,4 @@
-import React from "react";
+import { render } from "react-dom";
 import { App } from "./App";
+
+render(<h1>Test</h1>, document.getElementById("root")); 
